b21978d0eed552f867ed85b0ca0eb368a7357776
Juanes <jedp082@gmail.com>
2019-09-22 22:24:27 -0500

    tercer commit - InicioCompletado/Contacto - 22/09/2019

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import {Routes, RouterModule} from '@angular/router';
 
 //Componentes para las rutas
 import { IndexComponent } from '../app/components/outlet/index/index.component';
+import {ContactComponent} from '../app/components/outlet/contact/contact.component';
+import {IngresarComponent} from '../app/components/outlet/ingresar/ingresar.component';
 
 //Variable que permite las rutas dinamicas
 const routes: Routes = [
-  {path: 'index', component: IndexComponent}
+  {path: 'inicio', component: IndexComponent},
+  {path: 'contacto', component: ContactComponent },
+  {path: 'ingresar', component: IngresarComponent}
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,17 @@ import {AppRoutingModule} from './app-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { IndexComponent } from './components/outlet/index/index.component';
+import { ContactComponent } from './components/outlet/contact/contact.component';
+import { IngresarComponent } from './components/outlet/ingresar/ingresar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    IndexComponent
+    IndexComponent,
+    ContactComponent,
+    IngresarComponent
   ],
   imports: [
     BrowserModule,
