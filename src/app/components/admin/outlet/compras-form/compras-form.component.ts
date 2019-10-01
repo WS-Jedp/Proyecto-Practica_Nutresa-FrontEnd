f8ad081d75e97ac845af09f46d68b342705713ff
Juanes <jedp082@gmail.com>
2019-09-30 20:54:20 -0500

    15vo commit - formCompra - 30/09/2019

--- src/app/components/admin/outlet/compras-form/compras-form.component.ts
+++ src/app/components/admin/outlet/compras-form/compras-form.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-
+import {ClientesInterface} from '../../../../interfaces/clientes/clientes-interface';
 import {CompraProducto} from '../../../../interfaces/compras/compra-producto';
 import {ApiService} from '../../../../services/api.service';
+
+import {OneProduct} from '../../../../interfaces/productos/one-product';
+import {ProductoService} from '../../../../services/productos/producto.service';
 
 @Component({
   selector: 'app-compras-form',
@@ -15,15 +18,30 @@ export class ComprasFormComponent implements OnInit {
 
   compras:any[];
 
-  constructor(private apiService:ApiService) {
+  loadingProducto:boolean;
+  producto:object;
+
+  clientes:any;
+
+  combo:boolean;
+
+  constructor(private apiService:ApiService, private apiProducto:ProductoService) {
 
     this.isLoading = true;
+    this.loadingProducto = true;
+    this.combo = false;
 
     this.apiService.getCompra().subscribe(
       (resp:CompraProducto)=>{
         this.compras = resp.productos;
 
         this.isLoading = false;
+      })
+
+
+    this.apiService.getClients().subscribe(
+      (resp:ClientesInterface)=>{
+        this.clientes = resp.Clientes;
       }
     )
 
@@ -31,5 +49,25 @@ export class ComprasFormComponent implements OnInit {
 
   ngOnInit() {
   }
+
+// Seleccionar producto
+  comprarProducto(id){
+    this.apiProducto.getProduct(id).subscribe(
+      (resp:OneProduct)=>{
+        this.producto = resp.Producto;
+        this.loadingProducto = false;
+      })
+  }
+
+
+  // compra combo
+  compraCombo(){
+    this.combo = true;
+  }
+
+  compraDirecta(){
+    this.combo = false;
+  }
+
 
 }
